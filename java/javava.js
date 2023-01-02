f9afa12591fef3b309ce1fbe1ce9a32c8c8db098
sengwoong <rje287573@gmail.com>
2023-01-02 15:50:22 +0900

    first commit

---
+++ java/javava.js
@@ -0,0 +1,13 @@
+
+var div=[];
+var html = [];
+var i=0;
+div = div.join("<ul>")
+for (i;i<10;i++){
+ html = html.join("<li id=id"+i+">"+"id:"+i+"</li>")
+ 
+}
+console.log( div.toString().slice(1,1))
+
+
+document.body.innerHTML = result
